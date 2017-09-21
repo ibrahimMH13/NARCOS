0e62d563bd3eac084248f4cae286f0993830d69c
Ibrahim I Musabah <i.musabah92@gmail.com>
2017-09-21 18:35:58 +0300

    [105] Create view of reply

--- resources/views/Threads/show.blade.php
+++ resources/views/Threads/show.blade.php
@@ -15,23 +15,14 @@
 
         <div class="row">
                 @forelse($thread->replies as $reply)
-                    <div class="col-md-6 col-md-offset-2">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <a class="btn btn-link" href="#">{{$reply->user->name}}</a>
-                            {{'Since '.$reply->created_at->diffForHumans()}}
-                            </div>
-                            <div class="panel-body">
-                             {{$reply->body}}
-                            </div>
-                        </div>
-                    </div>
-                @empty
-                    <div class="col-md-8 col-md-offset-2">
-                        <p class="text-center text-capitalize alert alert-link">
-                            NO Reply Yet ,Be First !!
-                        </p>
-                    </div>
+                    @include('Threads.reply')
+            @empty
+                <div class="col-md-8 col-md-offset-2">
+                    <p class="text-center text-capitalize alert alert-link">
+                        NO Reply Yet ,Be First !!
+                    </p>
+                </div>
+
             @endforelse
         </div>
     </div>
