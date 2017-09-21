4c29769edb2701118b76ded18fc1a6a43fe95a78
Ibrahim I Musabah <i.musabah92@gmail.com>
2017-09-21 18:28:07 +0300

    [104] Define RelationShip between user and reply

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
     //
+
+
+    //RelationShip
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
 }
