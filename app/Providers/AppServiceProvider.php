96ed857e3778cc54503c8770f8395a6b33498dd0
Ibrahim I Musabah <i.musabah92@gmail.com>
2017-10-04 19:13:45 +0300

    [132] Install packge barrydh/laraveldebug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
             });
             $view->with("channels",$channels);
         });
+
+
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
          * Package Service Providers...
          */
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -229,6 +231,7 @@ return [
         //MyAlias
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
