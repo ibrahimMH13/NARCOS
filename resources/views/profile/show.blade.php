d937b1cecf4394897c715481e19014877943751a
Ibrahim I Musabah <i.musabah92@gmail.com>
2017-10-06 17:27:14 +0300

    [137] coustm view of prifle

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -13,16 +13,30 @@
                     </p>
                 </div>
             </div>
+            @forelse($profile->thread as $thread)
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Profiles</div>
+                    <div class="panel-heading">
+                     <a href="{{$thread->path()}}">
+                         <p class="level">
+                            <span class="flax">
+                                  {{$thread->title}}
+                            </span>
+                             <span>
+                                {{$thread->created_at->diffForHumans()}}
+                            </span>
+                         </p>
+                     </a>
 
+                    </div>
                     <div class="panel-body">
-
-
+                        {{$thread->body}}
                     </div>
                 </div>
             </div>
+                @empty
+                <p> No there Activity Yet</p>
+            @endforelse
         </div>
     </div>
 @endsection
